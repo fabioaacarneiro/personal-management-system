322c1476562aec03a866e8a480a49b4fb513afa5
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-08-20 15:53:31 -0300

    corrigido erro de rodas no LoginController

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     public function index() {
-        return view('login');
+        return view('home');
     }
 
     public function store() {
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 @extends('master')
 
+@section('Home')
+
 @section('content')
     <a href="{{ route('login.index') }}">Login</a>
     <h2>Home</h2>
