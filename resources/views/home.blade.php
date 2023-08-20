79cda6950d6d0ce86d69b76b4b6122a56b427927
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-08-20 15:37:51 -0300

    criado roda 'login.index' no arquivo web.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('master')
+
+@section('content')
+    <a href="{{ route('login.index') }}">Login</a>
+    <h2>Home</h2>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::controller(LoginController::class)->group(function () {
+    Route::get('/login', 'index')->name('login.index');
+})
