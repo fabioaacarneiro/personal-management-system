7a17a20860eb5488dcc1199e92372c7ee5b9dda0
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-08-20 15:39:44 -0300

    criado rota 'login.destroy'

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,5 @@ Route::get('/', [HomeController::class, 'index']);
 Route::controller(LoginController::class)->group(function () {
     Route::get('/login', 'index')->name('login.index');
     Route::post('/login', 'store')->name('login.store');
-})
+    Route::get('/login', 'destroy')->name('login.destroy');
+});
