6f4e9afc5918876fa1c52c78b84c0b992e7f1715
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-08-20 21:16:27 -0300

    criado método para atualização de senha no AccountSettings controller

--- app/Http/Controllers/AccountSettings.php
+++ app/Http/Controllers/AccountSettings.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 
 class AccountSettings extends Controller
 {
-    
+    public function update_password(Request $request, $id) {
+        try {
+            $user = User::findOrFail($id);
+        } catch (Exception) {
+            return 'Usuário inválido ou não existe no registro';
+        }
+
+        try {
+            if ($request->password == $user->password) {
+                if ($request->new_password == $request->new_password_confirm) {
+                    $user->update([
+                        'password' => $request->new_password
+                    ]);
+                } else {
+                    return 'as senhas não combinas.';
+                }
+            }
+        } catch (\Throwable $th) {
+            return 'não foi possível atualizar a senha,';
+        }
+    }
 }
