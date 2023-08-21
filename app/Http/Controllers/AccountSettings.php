969187f194de24b6c537b6b97112a09ae6a3a6a7
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-08-21 10:41:22 -0300

    implementados métodos de atualização de nome de usuário e reset de senhas

--- app/Http/Controllers/AccountSettings.php
+++ app/Http/Controllers/AccountSettings.php
@@ -29,12 +29,35 @@ class AccountSettings extends Controller
     }
 
     public function update_name(Request $request, $id) {
-        $user = User::findOrFail($id);
+        try {
+            $user = User::findOrFail($id);
+        } catch (Exception) {
+            return 'Usuário inválido ou não existe no registro';
+        }
 
         try {
             $user->update(['name' => $request->new_name]);
         } catch (Exception) {
             return 'Não foi possível atualizar o nome do usuário';
+        }
+    }
+
+    public function reset_password(Request $request, $id) {
+        try {
+            $user = User::findOrFail($id);
+        } catch (Exception) {
+            return 'Usuário inválido ou não existe no registro';
+        }
+
+        try {
+            if ($request->confirm_reset == "resetar" && $request->email == $user->email) {
+                $new_password_token = rand(10000000, 9999999);
+                $user->update(['email' => $new_password_token]);
+            } else {
+                return 'E-Mail ou confirmação inválidos, revise por favor.';
+            }
+        } catch (Exception) {
+            return 'Não foi possível resetar a senha.';
         }
     }
 }
